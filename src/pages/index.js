ebfa637705114f16772dbe853cf2ceb8fdce2b4e
TahaAlam <tahaalam@buffalo.edu>
2020-01-15 01:44:22 -0500

    added pictures,  made black background, changed mountain picture to the fraternity flag

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,17 @@ import Helmet from "react-helmet";
 
 import Layout from '../components/layout';
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
+import purpose from '../assets/images/Purpose.jpg'
+import brotherhood from '../assets/images/Brotherhood.jpg'
+import pd from '../assets/images/ProfessionalDevelopment.jpg'
+import commserv from '../assets/images/CommunityService.jpg'
+
+import R from '../assets/images/R.jpg'
+import VR from '../assets/images/VR.jpg'
+import Treasurer from '../assets/images/Treasurer.jpg'
+import CorrSec from '../assets/images/CorrSec.jpg'
+import PM from '../assets/images/PM.jpg'
+import Scribe from '../assets/images/Scribe.jpg'
 
 class Homepage extends React.Component {
     render() {
@@ -23,36 +30,16 @@ class Homepage extends React.Component {
                             <p>The purpose of Theta Tau is to develop and maintain a high standard of professional interest among its members, and to unite them in a strong bond of fraternal fellowship.</p>
                         </div>
                         <div className="col-6">
-                            <span className="image fit"><img src={pic01} alt="" /></span>
+                            <span className="image fit"><img src={purpose} alt="" /></span>
                         </div>
                     </div>
                 </section>
 
-                <section id="two" className="main style2">
-                    <div className="grid-wrapper">
-                        <div className="col-6">
-                            <ul className="major-icons">
-                                <li><span className="icon style1 major fa-code"></span></li>
-                                <li><span className="icon style2 major fa-bolt"></span></li>
-                                <li><span className="icon style3 major fa-camera-retro"></span></li>
-                                <li><span className="icon style4 major fa-cog"></span></li>
-                                <li><span className="icon style5 major fa-desktop"></span></li>
-                                <li><span className="icon style6 major fa-calendar"></span></li>
-                            </ul>
-                        </div>
-                        <div className="col-6">
-                            <header className="major">
-                                <h2>Lorem ipsum dolor adipiscing<br />
-                                amet dolor consequat</h2>
-                            </header>
-                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
-                            <p>Blandit faucibus proin. Ac aliquam integer adipiscing enim non praesent vis commodo nunc phasellus cubilia ac risus accumsan. Accumsan blandit. Lobortis phasellus non lobortis dit varius mi varius accumsan lobortis. Blandit ante aliquam lacinia lorem lobortis semper morbi col faucibus vitae integer placerat accumsan orci eu mi odio tempus adipiscing adipiscing adipiscing curae consequat feugiat etiam dolore.</p>
-                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
-                        </div>
-                    </div>
+                <section  className="main style2 special">
+              <div><br/><br/><br/></div> 
                 </section>
 
-                <section id="three" className="main style1 special">
+                <section id="two" className="main style1 special">
                     <div className="grid-wrapper">
                         <div className="col-12">
                             <header className="major">
@@ -61,7 +48,7 @@ class Homepage extends React.Component {
                         </div>
 
                         <div className="col-4">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
+                            <span className="image fit"><img src={brotherhood} alt="" /></span>
                             <h3>Brotherhood</h3>
                             <p>We forge lifelong bonds of fraternal friendship, a journey that develops and delivers a network of lasting personal and professional relationships.  We foster an inviting, safe, and social environment in which our members become lifelong friends.</p>
                             <ul className="actions">
@@ -69,7 +56,7 @@ class Homepage extends React.Component {
                             </ul>
                         </div>
                         <div className="col-4">
-                            <span className="image fit"><img src={pic03} alt="" /></span>
+                            <span className="image fit"><img src={pd} alt="" /></span>
                             <h3>Professionalism</h3>
                             <p>We develop and nurture engineers with strong communication, problem-solving, collaboration, and leadership skills that we demonstrate in our profession, our community, and in our lives.</p>
                             <ul className="actions">
@@ -78,7 +65,7 @@ class Homepage extends React.Component {
                         </div>
                         
                         <div className="col-4">
-                            <span className="image fit"><img src={pic04} alt="" /></span>
+                            <span className="image fit"><img src={commserv} alt="" /></span>
                             <h3>Community Service</h3>
                             <p>We are known for our service to our college, university and the larger community.  Our service projects create a unifying environment for learning and personal growth for our members.</p>
                             <ul className="actions">
@@ -88,7 +75,12 @@ class Homepage extends React.Component {
 
                     </div>
                 </section>
-                <section id="four" className="main style1 special">
+
+                <section  className="main style2 special">
+              <div><br/><br/><br/></div> 
+                </section>
+
+                <section id="three" className="main style1 special">
                     <div className="grid-wrapper">
                         <div className="col-12">
                             <header className="major">
@@ -97,39 +89,33 @@ class Homepage extends React.Component {
                         </div>
                         <div className="col-2"> </div>
                         <div className="col-4">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Regent</h3>   
+                            <h5><span className="image fit"><img src={R} alt="" /></span> Regent <br /> Eugene Liang</h5>  
                         </div>
                         <div className="col-4">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Vice-Regent</h3>   
+                            <h5><span className="image fit"><img src={VR} alt="" /></span> Vice-Regent <br /> Vincent Tricozzi </h5>
                         </div>
                         <div className="col-2"> </div>
                         <div className="col-3">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Pledge Marshall</h3>   
+                            <h5><span className="image fit"><img src={PM} alt="" /></span> Pledge Marshall <br /> Hollis Pauquette </h5>    
                         </div>
                         <div className="col-3">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Corresponding Secretary</h3>   
+                            <h5><span className="image fit"><img src={CorrSec} alt="" /></span> Corresponding Secretary <br/> Taylor Marabella </h5>      
                         </div>
                         <div className="col-3">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Treasurer</h3>   
+                            <h5><span className="image fit"><img src={Treasurer} alt="" /></span> Treasurer <br/> Casey Dolan </h5>    
                         </div>
                         <div className="col-3">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Scribe</h3>   
+                           <h5> <span className="image fit"><img src={Scribe} alt="" /></span> Scribe <br/> Joshua Hulburt  </h5>   
                         </div>
 
                     </div>
                 </section>
-                <section id="five" className="main style2 special">
+                <section id="four" className="main style2 special">
                     <div className="container">
                         <header className="major">
-                            <h2>Ipsum feugiat consequat?</h2>
+                            <h2>Interested in rushing?</h2>
                         </header>
-                        <p>Sed lacus nascetur ac ante amet sapien.</p>
+                        <p>We will keep you updated.</p>
                         <ul className="actions uniform">
                             <li><a href="#" className="button special">Sign Up</a></li>
                             <li><a href="#" className="button">Learn More</a></li>
